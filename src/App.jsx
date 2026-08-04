618dcdd6f038fd26ff3fa318b3726e24381a070d
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-04 15:15:59 +1000

    Added CoinCard component and display coins

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import CoinCard from "./components/CoinCard";
 const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
 const App = () => {
@@ -8,20 +9,24 @@ const App = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => {
-        if(!res.ok) throw new Error('Failed to fetch data.');
-        return res.json();
-      })
-      .then((data) => {
+    // to create async function
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if(!res.ok) throw new ERROR('Failed to fetch data.');
+        const data = await res.json();
         console.log(data);
         setCoins(data);
-        setLoading(false);
-      })
-      .catch((err) => {
+      } catch (err) {
         setError(err.message);
+      } finally {
         setLoading(false);
-      });
+      }
+    }
+
+    // to call async function
+    fetchCoins();
+
   }, []);
 
   
@@ -29,6 +34,16 @@ const App = () => {
   return ( 
     <div>
       <h1>🚀 Crypto Dash</h1>
+      { loading && <p>Loading...</p> }
+      { error && <div className="error">{error}</div> }
+
+      {!loading && !error && (
+       <main className="grid">
+        {coins.map((coin) => (
+          <CoinCard coin={coin} key={coin.id} />
+        ))}
+       </main> 
+      )}
     </div>
    );
 }

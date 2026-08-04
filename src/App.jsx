7cbbe0ddfa3f42e38ce24ae14de9e1788d9c1ff8
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-04 20:19:21 +1000

    Added environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
-const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
 
@@ -12,7 +12,7 @@ const App = () => {
     // to create async function
     const fetchCoins = async () => {
       try {
-        const res = await fetch(API_URL);
+        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`);
         if(!res.ok) throw new ERROR('Failed to fetch data.');
         const data = await res.json();
         console.log(data);

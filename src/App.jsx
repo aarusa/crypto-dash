b3c87ddeb3736e053a98450a487ea72bc1389564
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-05 10:52:30 +1000

    Limit control added

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
 
   useEffect(() => {
     // to create async function
     const fetchCoins = async () => {
       try {
-        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`);
+        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`);
         if(!res.ok) throw new ERROR('Failed to fetch data.');
         const data = await res.json();
         console.log(data);
@@ -27,7 +28,7 @@ const App = () => {
     // to call async function
     fetchCoins();
 
-  }, []);
+  }, [limit]);
 
   
 
@@ -36,6 +37,17 @@ const App = () => {
       <h1>🚀 Crypto Dash</h1>
       { loading && <p>Loading...</p> }
       { error && <div className="error">{error}</div> }
+
+      <div className="controls">
+        <label htmlFor="limit">Show:</label>
+        <select value={limit} id="limit" onChange={(e) => setLimit(Number(e.target.value))}>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
+      </div>
 
       {!loading && !error && (
        <main className="grid">

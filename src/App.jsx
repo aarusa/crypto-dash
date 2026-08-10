7ca51abc4967efdd5e568e75ccbd584521b77499
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-10 13:06:30 +1000

    Adding an about page and a header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
-import HomePage from "./pages/home";
 import { Routes, Route } from 'react-router';
+import Header from "./components/Header";
+import HomePage from "./pages/home";
+import AboutPage from "./pages/about";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -36,19 +38,23 @@ const App = () => {
 
 
   return ( 
-    <Routes>
-      <Route path='/' element={ <HomePage 
-          coins={coins} 
-          filter={filter}
-          setFilter={setFilter}
-          limit={limit}
-          setLimit={setLimit}
-          sortBy={sortBy}
-          setSortBy={setSortBy}
-          loading={loading}
-          error={error}
-        />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={ <HomePage 
+            coins={coins} 
+            filter={filter}
+            setFilter={setFilter}
+            limit={limit}
+            setLimit={setLimit}
+            sortBy={sortBy}
+            setSortBy={setSortBy}
+            loading={loading}
+            error={error}
+          />} />
+          <Route path='/about' element={ <AboutPage/> } />
+      </Routes>
+    </>
    );
 }
  
---
+++ src/pages/about.jsx
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+    return ( 
+        <div className="about">
+            <h1>About Crypto Dash</h1>
+            <p>Crypto Dash is a simple and interactive cryptocurrency dashboard built to make crypto market information easy to explore. It provides live updates on cryptocurrency prices, market changes, and other key details in one place.</p>
+
+            <p>The project focuses on creating a clean, responsive experience while working with real-time API data and dynamic UI updates. It is designed to help users quickly understand market movements without unnecessary complexity.</p>
+
+            <p>Crypto Dash also emphasizes performance and usability, ensuring that data is updated smoothly and presented in a clear, organized layout. The interface is built to be beginner-friendly while still being useful for more experienced crypto users.</p>
+
+            <p>Overall, this project reflects my interest in web development and working with live data systems, combining both functionality and design in a practical dashboard application.</p>
+        
+        </div>
+     );
+}
+ 
+export default AboutPage;

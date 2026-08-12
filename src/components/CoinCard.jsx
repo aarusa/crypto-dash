c525c0b89efb152fe47e64a6580677dc47ed757c
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-12 23:19:38 +1000

    Coins detail page

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router";
+
 const CoinCard = ({coin}) => {
     return ( 
+      <Link to={`/coin/${coin.id}`} >
         <div className="coin-card">
             <div className="coin-header">
               <img src={coin.image} alt={coin.name} className="coin-image" />
@@ -12,6 +15,7 @@ const CoinCard = ({coin}) => {
             <p className={ coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}>{coin.price_change_percentage_24h.toFixed(2)} %</p>
             <p>Market cap: {coin.market_cap.toLocaleString()}</p>
         </div>
+      </Link>
      );
 }
  

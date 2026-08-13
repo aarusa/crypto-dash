dba517f410e05feb351f0e3d67dbd97727903738
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-08-13 11:54:43 +1000

    Adding React Spinner

--- src/pages/coin-details.jsx
+++ src/pages/coin-details.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router";
+import Spinner from "../components/Spinner";
 
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
@@ -37,7 +38,7 @@ const CoinDetailsPage = () => {
                 { coin ? `${coin.name} (${coin.symbol.toUpperCase()})` : 'Coin Details'}
             </h1>
 
-            { loading && <p>Loading...</p> }
+            { loading && <Spinner /> }
             { error && <div className="error">{error}</div> }
 
             { !loading && !error && 
